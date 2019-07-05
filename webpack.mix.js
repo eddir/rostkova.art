b5f3d50c95cf044645911148b06c39a81449b878
Eduard Rostkov <eddirworkmail@gmail.com>
2019-07-05 19:07:45 +0200

    Исправление ошибок совместимости с Node.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .options({
+       processCssUrls: false
+   });
